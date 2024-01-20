8c2a26350787f80aadf8ca5fcd8098d173709ac7
Moderocky <admin@moderocky.com>
2024-01-20 18:14:05 +0000

    Make rarity easier to print.

--- src/main/java/mx/kenzie/blocklike/Rarity.java
+++ src/main/java/mx/kenzie/blocklike/Rarity.java
@@ -1,10 +1,29 @@
 package mx.kenzie.blocklike;
 
-public enum Rarity {
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.ComponentLike;
+import net.kyori.adventure.text.format.TextColor;
+import org.jetbrains.annotations.NotNull;
 
-    COMMON,
-    RARE,
-    EPIC,
-    LEGENDARY
+public enum Rarity implements ComponentLike {
 
+    COMMON(TextColor.color(194, 194, 194)),
+    RARE(TextColor.color(0, 170, 255)),
+    EPIC(TextColor.color(174, 0, 255)),
+    LEGENDARY(TextColor.color(255, 60, 0));
+    public final TextColor color;
+
+    Rarity(TextColor color) {
+        this.color = color;
+    }
+
+    public TextColor color() {
+        return color;
+    }
+
+    @Override
+    public @NotNull Component asComponent() {
+        final String name = this.name().toLowerCase().replace('_', ' ');
+        return Component.translatable("rarity." + name, name.substring(0, 1).toUpperCase() + name.substring(1), color);
+    }
 }
